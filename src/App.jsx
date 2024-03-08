97ced46b143a16a86e7b6f630d9332ec9e608c03
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-03-08 12:29:46 +0100

    countryparent and child

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
 import { useState, useEffect, useContext, createContext } from 'react'
 import './App.css'
 import CountryParent from './components/CountryParent'
-// import {  Route,  RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
-export const countryContext = createContext()
+import CountryChild from './components/CountryChild'
+import {  RouterProvider, createBrowserRouter } from "react-router-dom"
+export const countryContext = createContext(null)
+import axios from 'axios'
 
 function App() {
   const[data, setData] = useState([])
+  useEffect(() => {
+      axios.get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  
+    },[]);
+
+  const router = createBrowserRouter([
+    {
+      path:'/', 
+      element:(
+        <countryContext.Provider value={{data, setData}}>
+          <CountryParent></CountryParent>
+        </countryContext.Provider>
+        ),
+    },
+    {
+    path:'/country/:id', 
+      element:(
+      <countryContext.Provider value={{data, setData}}>
+        <CountryChild></CountryChild>
+      </countryContext.Provider>
+      ),
+    },
+  ])
 
   return (
    <div>
-    <countryContext.Provider value={{data, setData}}>
-      <CountryParent></CountryParent>
-    </countryContext.Provider>
-
+    <RouterProvider router={router}></RouterProvider>
+    {/* <CountryParent></CountryParent> */}
    </div>
   )
 }
-
 export default App
--- src/components/CountryChild.jsx
+++ src/components/CountryChild.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import { countryContext } from '../App'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function CountryChild() {
+    const {data, setData} = useContext(countryContext)
+    const {id} = useParams()
+    
+    // useEffect(()=>{
+    //     setData(countryContext)
+    // }, [])
+    const dataCountry = data[id]
+    // console.log('salut',data);
+
   return (
-    <div>CountryChild</div>
+    <div className='w-screen flex flex-col gap-[20px] justify-center items-center p-[20px]'>
+        <div className='flex justify-center items-center'>
+        <Link to={`/`}>
+            <button className='border border-black font-[Barlow] bg-slate-300 p-[20px] justify-start items-start'>Back to countries</button>
+        </Link>
+        </div>
+        <div className='flex flex-col w-[90%] justify-center items-center'>
+            <div className='flex justify-center w-[50%]'>
+                <img className='w-[130px] h-[100px] rounded-lg border border-black' src={dataCountry.flags.png} alt=""/>
+            </div>
+            <div className='flex flex-col justify-center items-center gap-[10px]'>
+                <h1 className='w-[70%] text-[30px] text-center'>{dataCountry.name.common}</h1>
+                
+                <div className='flex flex-col border border-black p-[30px] gap-[5px]'>
+                    <p>Official name: {dataCountry.name.official}</p>
+                    <p>Population : {dataCountry.population}</p> 
+                    <p>Region : {dataCountry.region}</p>
+                    <p>Sub Region : {dataCountry.subregion}</p>
+                    <p>Capital : {dataCountry.capital}</p>
+                    <p>Top Level Domain : {dataCountry.tld}</p>
+                    <p>Currencies: </p>
+                    <p></p>
+                    {/* {Object.keys(dataCountry.currencies).map((key, index)=>(
+                        <li>{dataCountry.currencies[key]}</li>
+                    ))} */}
+                    
+                    {/* <p>Languages:</p>
+                    {Object.values(dataCountry.languages).map((key, index)=>(
+                        <li key={index}>{dataCountry.languages[key]}</li>
+                    ))} */}
+                </div>
+            </div>
+        </div>
+        <div>
+            <h1>Border Countries: </h1>
+        </div>
+    </div>
   )
 }
